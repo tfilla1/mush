0f59b8586e7f3c53f30cd0b767ee36b5986a0a6c
Tom Filla <tfilla1@gmail.com>
2022-07-06 07:49:16 -0600

    updating mush

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-var express = require('express'); 
+var express = require('express');
+var httpProxy = require('http-proxy');
+ 
+
+
+
 var app = express();
+var port = 3032;
+
+app.use(express.static(__dirname + '/public'));
+httpProxy.createProxyServer({target:'http://localhost:8081'}).listen(port); // See (†)
+ 
+//create proxy to localhost:8080
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/web/public/dist/index.html');
+// })
+
+//app.listen(port, () => { console.log(`catch it on port: ${port}!`); })
